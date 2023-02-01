bcf818470283c0bd1f43908ffa8dd7b9eda3f3dc
lis0jk <lissjkk@gmail.com>
2023-02-01 14:01:51 +0300

    Sreate rules modal and download some files

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,24 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  function openForm() {
+    const formRulesBtn = document.querySelectorAll(".rule-btn");
+    const closeRulesFormBtn = document.querySelectorAll(".modal__close");
+    const modalRulesForm = document.querySelector(".modal-rule");
+
+    formRulesBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modalRulesForm.style.display = "block";
+      });
+    });
+
+    closeRulesFormBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modalRulesForm.style.display = "none";
+      });
+    });
+  }
+  openForm();
+});
